133c58eeb7f77489ac914d82ec0af93215f5ba54
Mltsua-piya <priyawankhade0314@gmail.com>
2026-08-08 23:44:54 +0530

    fixed All Listing route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
-const ejsMate=require("ejs-mate");
+const ejsMate = require("ejs-mate");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 
@@ -35,15 +35,15 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am Groot");
 });
 
 //Index Route
-app.get("/listing", async (req, res) => {
+app.get("/listings", async (req, res) => {
   const allListing = await Listing.find({});
   res.render("listings/index", { allListing });
 });
@@ -65,7 +65,7 @@ app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.Listing);
 
   await newListing.save();
-  res.redirect("/listing");
+  res.redirect("/listings");
 });
 //edit route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -80,11 +80,11 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect("/listings/show");
 });
 //delete route
-app.delete("/listings/:id",async (req,res)=>{
-  let {id}=req.params;
-  let deletedListing=await Listing.findByIdAndDelete(id);
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
-  res.redirect("/listing");
+  res.redirect("/listings");
 });
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
