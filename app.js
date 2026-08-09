f98fc959e03db3b88d1740bdf94da26690f66b5f
Mltsua-piya <priyawankhade0314@gmail.com>
2026-08-09 23:27:07 +0530

    added styling for show page

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.get("/listings/:id/edit", async (req, res) => {
 app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.Listing });
-  res.redirect("/listings/show");
+  res.redirect(`/listings/${id}`);
 });
 //delete route
 app.delete("/listings/:id", async (req, res) => {
