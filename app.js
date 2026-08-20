55340b52478701135e268196a13883ba2165b8a6
Mltsua-piya <priyawankhade0314@gmail.com>
2026-08-20 22:50:58 +0530

    Add review creation validation display and deletion

--- app.js
+++ app.js
@@ -131,6 +131,15 @@ app.post(
     res.redirect(`/listings/${listing._id}`);
   }),
 );
+
+//delete review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+  let {id ,reviewId}=req.params;
+  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`);
+}));
 app.all("/{*splat}", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found !"));
 });
