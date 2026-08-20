b3687c0a740dfb0ed1fe375ce3906654c4a5bb81
Mltsua-piya <priyawankhade0314@gmail.com>
2026-08-20 17:48:42 +0530

    creted review model for listing

---
+++ models/review.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const reviewSchema=new Schema({
+    commnet:String,
+    rating:{
+        type:Number,
+        min:1,
+        max:5
+    },
+    createdAt:{
+        type:Date,
+        deafault:Date.now(),
+    },
+});
+module.exports=mongoose.model("Review",reviewSchema);
