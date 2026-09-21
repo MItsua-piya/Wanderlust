26daac3a170e738cc943a6a7a2cf56f76acb9d3f
Mltsua-piya <priyawankhade0314@gmail.com>
2026-09-21 11:22:46 +0530

    flash messages for insertion , deletion and updation of listing and reviews are added

--- routes/listing.js
+++ routes/listing.js
@@ -46,7 +46,7 @@ router.post(
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    req.flash("sucess","New Listing Created");
+    req.flash("success","New Listing Created");
     res.redirect("/listings");
   }),
 );
@@ -68,6 +68,7 @@ router.put(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+     req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`);
   }),
 );
@@ -79,6 +80,7 @@ router.delete(
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+     req.flash("success"," Listing Deleted");
     res.redirect("/listings");
   }),
 );
