106646d93179b8485c18a5cf475202e224a672da
Mlsua-piya <priyawankhade0314@gmail.com>
2026-09-23 18:45:35 +0530

    Add automatic login after signup

--- routes/user.js
+++ routes/user.js
@@ -6,14 +6,21 @@ const wrapAsync = require("../utils/wrapAsycn.js");
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
+
 router.post("/signup", wrapAsync(async (req, res) => {
 try{
       let { username, email, password } = req.body;
   const newUser = new User({ email, username });
   const registeredUser = await User.register(newUser, password);
   console.log(registeredUser);
-  req.flash("success", "Welcome to Wanderlust");
+  req.login(registeredUser,(err)=>{
+ if(err){
+  return next(err);
+ }
+   req.flash("success", "Welcome to Wanderlust");
   res.redirect("/listings");
+  })
+
 }catch(e){
 req.flash("error",e.message);
 res.redirect("/signup");
@@ -36,4 +43,5 @@ router.get("/logout",(req,res)=>{
     req.redirect("/listings");
   })
 })
+
 module.exports = router;
